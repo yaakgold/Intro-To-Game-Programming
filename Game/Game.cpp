56bea823fd34a6247946ea168e312469eb12dbf2
yaakgold <yaakovgoldberg007@gmail.com>
2020-08-03 13:08:26 -0400

    Day 1: Setting up the window, and setting up the project

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int, char**)
 {
-    
-}
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "SDL_INIT Error." << SDL_GetError() << std::endl;
+		return 1;
+	}
 
+	SDL_Window* window = SDL_CreateWindow("GAT 150", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cout << "SDL_CreateWindow Error. " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	std::getchar();
+
+	SDL_Quit();
+
+	return 0;
+}
